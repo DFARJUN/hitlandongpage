998d417346081cce277a5426db6931f9c71ba78f
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2019-05-17 15:03:00 +0300

    1705

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,5 @@
+﻿var wind = window.innerHeight;
+var wind2 = window.innerWidth;
+document.documentElement.style.setProperty('--height-win', wind + "px");
+document.documentElement.style.setProperty('--width-win', wind2 + "px");
+
